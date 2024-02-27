ba4d19970152eb73b6044213215d5273b4ca0bf7
Marcus Mathuram <marcus.mathuram7@gmail.com>
2024-02-27 19:41:32 +0530

    Completed Intro

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,29 +1,50 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Intro.css";
 function Intro() {
   const [titleText, setTitleText] = useState("MARCUS");
   const [fsdText, setFsdText] = useState("FULL STACK DEVELOPER (MERN)");
 
-  // const animateTitle = (text, setText) => {
-  //   setText('');
-  //   text.split('').forEach((letter, index) => {
-  //     setTimeout(() => {
-  //       setText(prevText => prevText + letter);
-  //     }, index * 500);
-  //   })
-  // }
+  const animateTitle = (text, setText) => {
+    setText('');
+    text.split('').forEach((letter, index) => {
+      setTimeout(() => {
+        setText(prevText => prevText + letter);
+      }, index * 500);
+    });
+  };
+
+  useEffect(() => {
+    animateTitle("MARCUS", setTitleText);
+    // animateTitle('FULL STACK DEVELOPER (MERN)', setFsdText);
+
+    const intervalId = setInterval(() => {
+      animateTitle("MARCUS", setTitleText);
+      // animateTitle('FULL STACK DEVELOPER (MERN)', setFsdText);
+    }, 4000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const handleResumeClick = (event) => {
+    const resumeUrl = 'https://drive.google.com/file/d/1awZo4H7LHvnMw7TG-h47xeuzoLabwOmn/view?usp=drivesdk ';
+
+    window.open(resumeUrl, '_blank');
+  }
 
   return (
-      <section>
-        <div>
-        <h1>Hi,I'm Marcus Mathuram</h1>
+      <section id='intro'>
+        <div id='left-content'>
+          <h1 id='title'>Hello, I'm {titleText}</h1><br/>
+          <h1 id='fsd'>{fsdText}</h1>
+          <p id='para'>
+            I'm very much interested in creating websites and apps using MERN Stack (MongoDB, Express.js, React, Node.js)
+          </p>
+          <button className='resume' onClick={handleResumeClick}>View Resume</button>
         </div>
-        <div>
-          <h1>Full Stack Developer(MERN)</h1>
-        </div>
-        <div>
-          <h2>
-          I'm very much Interested in creating websites and apps using MERN Stack (MongoDB, Express.js, React, Node.js)</h2>
+        <div id='right-content'>
+          <div id='end'>
+            <img src='' alt='' className='rounded-image' />
+          </div>
         </div>
       </section>
   )
